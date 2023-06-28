8c8ed2ce2101525323914c2f54812299dcb8d74f
David <62346025+aboutdavid@users.noreply.github.com>
2023-06-28 02:21:02 -0400

    fix annoying line placement

--- matrix/sbpostgres/initdb.sh
+++ matrix/sbpostgres/initdb.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE DATABASE sb;
+    CREATE DATABASE sb;
     CREATE USER postgres WITH PASSWORD 'postgres';
     GRANT ALL PRIVILEGES ON DATABASE sb to postgres;
 EOSQL
